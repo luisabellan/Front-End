abe24327ccd672031f551b74ab82c9f54ee4a883
Nurudeen <nurudeen.amedu@gmail.com>
2019-09-23 23:27:01 +0100

    header and footer added

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import home from '../assets/home.png';
+import saved from '../assets/save.png';
+import book from '../assets/book.png';
+import '../style/header.css';
+
+const Header = props => (
+  <div className="header">
+    <img src={logo} className="logo" alt="logo" />
+    <div className="nav-link">
+
+      <NavLink className="link" key={'001'} to={'/'}>
+        <img src={home} className="link-icon" alt="logo" />
+        <div>Home</div>
+      </NavLink>
+
+      <NavLink className="link" key={'002'} to={'/saved'}>
+        <img src={saved} className="link-icon" alt="logo" />
+        <div>My Books</div>
+      </NavLink>
+
+      <NavLink className="link" key={'003'} to={'/saved'}>
+        <img src={book} className="link-icon" alt="logo" />
+        <div>Top Books</div>
+      </NavLink>
+    </div>
+  </div>
+);
+
+export default Header;
