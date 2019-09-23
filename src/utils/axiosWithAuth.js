0de2ba0dd311ded718f794910621de9a959bc625
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-09-23 11:20:47 -0700

    basic login

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,1 +1,12 @@
-//axios with authorization header
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+  
+    return axios.create({
+      baseURL: 'https://better-reads-bw.herokuapp.com/api/auth',
+      headers: {
+        Authorization: token
+      }
+    });
+  };
