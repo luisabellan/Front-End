32b153af0362f3e6898fe1de1bd0f6778aaca72b
Nurudeen <nurudeen.amedu@gmail.com>
2019-09-25 00:19:59 +0100

    search box completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import '../style/dashboard.css';
 
 const Dashboard = props => (
-  <div className="dashboard">
+  <div className="dashboard">  
       <form>
         <input type="text" className="textbox" placeHolder="Search"></input>
         <input title="Search" value="⚡ Let's Go!" type="submit" className="button"></input>
