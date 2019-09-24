9d3fb36c048c67286ad81ae1e21b919377a94edf
Nurudeen <nurudeen.amedu@gmail.com>
2019-09-24 01:29:22 +0100

    particles js added to the background

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Particles from 'react-particles-js';
+import particles from './components/particles';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+        <Particles params={particles}/>
       <Footer className="footer"></Footer>
     </div>
   );
