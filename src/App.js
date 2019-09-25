be3992955e44cea712ea0b80a28b9c86c7f142bd
Emma Zarate <zarate.emm@gmail.com>
2019-09-25 10:58:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import logo from './logo.svg';
+//Routes
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
+import LandingPage from './components/LandingPage/LandingPage';
+import Home from './components/Home';
+
+//Particles.js
 import Particles from 'react-particles-js';
 import particles from './components/particles';
 
-import { Route } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-import PrivateRoute from './components/PrivateRoute';
-import { Register } from './components/Register';
+//Dashboard
 import Dashboard from './components/Dashboard';
 import TopBooks from './components/TopBooks';
 
@@ -18,11 +22,13 @@ function App() {
     <div className="App">
 
       <Header></Header>
-        <Particles params={particles}/>
+      <Particles params={particles}/>
       <Footer className="footer"></Footer>
+      <Switch>
+        <PrivateRoute exact path='/home' component={Home} />
+        <Route exact path = '/' component={LandingPage} />
+      </Switch>
 
-      <PrivateRoute exact path='/register' component={Register}/>
-      <Route exact path ='/' component={Register} />
       <Route exact path ='/' component={Dashboard} />
       <Route path ='/topbooks' component={TopBooks} />
     </div>
