08da9ea4ddd3543255d0c53b07db702c4412d67e
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-26 16:46:31 -0500

    'updated styling and logout functionality'

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-const Logout = () => {
+const Logout = (props) => {
 
     const handleLogout = () => {
         localStorage.removeItem('token');
         window.location.reload();
-
-        //props.history.push('/')
+        props.history.push('/')
     }
 
     return(
         <button onClick={handleLogout}>Logout</button>
     )
 }
+
+export default Logout;
